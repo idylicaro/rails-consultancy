d34d960e1ceb0381b7dcfd08bef43778277c86b0
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-12 00:56:40 -0300

    test(consultant): ensure validations

--- spec/models/consultant_spec.rb
+++ spec/models/consultant_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Consultant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_numericality_of(:rating) }
+  end
 end
