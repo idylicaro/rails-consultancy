75c3637c1c3534c6b146bed4eff6a938819ec026
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-12 00:39:15 -0300

    feat(consultant): generate consultant model

---
+++ db/migrate/20201212033815_create_consultants.rb
@@ -0,0 +1,10 @@
+class CreateConsultants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :consultants do |t|
+      t.string :name
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
