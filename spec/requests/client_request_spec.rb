dafbefc7da3edc48394704e37a0d58511d95326e
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-14 00:47:27 -0300

    test(client_controller): test index and create routes

--- spec/requests/client_request_spec.rb
+++ spec/requests/client_request_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe "Clients", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/client/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /" do
+    it "returns all clients and http success" do
+      get "/client/"
+      expect(response).to have_http_status(:ok)
+    end
+    end
+  describe "POST /" do
+    it "returns http created" do
+      stub_name = Faker::name
+      # Todo: use factory
+      post "/client/", params: {name:stub_name}
+      expect(response).to have_http_status(:created)
     end
   end
 
