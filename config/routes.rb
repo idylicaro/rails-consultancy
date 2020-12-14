a4d11778df9cd7eb45640fde26561ede1d4d758e
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-14 00:09:55 -0300

    refactor(routes): use resources  for create rest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'client/index'
-  resources :client, only: [:index]
+  # 'resources :items' mapping (REST) routes
+  # GET    /items        #=> index
+  # GET    /items/1      #=> show
+  # GET    /items/new    #=> new
+  # GET    /items/1/edit #=> edit
+  # PUT    /items/1      #=> update
+  # POST   /items        #=> create
+  # DELETE /items/1      #=> destroy
+  
+  resources :client, only:[:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
