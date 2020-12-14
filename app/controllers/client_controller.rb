69f0e9e989486e0f42722f637bcd257775a0d509
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-14 00:10:51 -0300

    feat(client_controller): add create method

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -2,4 +2,22 @@ class ClientController < ApplicationController
   def index
     render json: Client.all
   end
+
+  def create
+    #client = Client.new(name: params[:name])
+    client = Client.new(client_params)
+
+    if client.save
+      render json: client, status: :created
+    else
+      render json: client.errors, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name)
+  end
 end
