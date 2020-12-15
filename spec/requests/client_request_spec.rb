dd6c162fa2ed49186a2c413142425769cdd186bc
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-14 22:38:25 -0300

    test(client_controller): ensure return json of client

--- spec/requests/client_request_spec.rb
+++ spec/requests/client_request_spec.rb
@@ -4,12 +4,26 @@ require 'faker'
 RSpec.describe "Clients", type: :request do
 
   describe "GET /" do
-    it "returns all clients and http success" do
+    before do
+      Client.create(name:'guest') #todo use factory
       get "/client/"
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:ok)
     end
+    it "JSON body response contains expected recipe attributes" do
+      json_response = JSON.parse(response.body)
+      expect(json_response[0].keys).to match_array(['id', 'name', 'created_at', 'updated_at'])
     end
+  end
   describe "POST /" do
+    it "returns http created" do
+      stub_name = Faker::name
+      # Todo: use factory
+      post "/client/", params: {name:stub_name}
+      expect(response).to have_http_status(:created)
+      end
     it "returns http created" do
       stub_name = Faker::name
       # Todo: use factory
