b385c982d613c1015d29159c1cd78b3c296fe2de
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-15 00:46:26 -0300

    test(client_controller): ensure destroy action and route

--- spec/requests/client_request_spec.rb
+++ spec/requests/client_request_spec.rb
@@ -3,21 +3,24 @@ require 'faker'
 
 RSpec.describe "Clients", type: :request do
 
-  describe "GET /" do
+  describe "GET /client" do
     before do
-      Client.create(name:'guest') #todo use factory
+      FactoryBot.create(:client)
+      FactoryBot.create(:client)
       get "/client/"
     end
 
-    it "returns http success" do
+    it "returns all clients" do
       expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body).size).to eq(2)
     end
+
     it "JSON body response contains expected recipe attributes" do
       json_response = JSON.parse(response.body)
       expect(json_response[0].keys).to match_array(['id', 'name', 'created_at', 'updated_at'])
     end
   end
-  describe "POST /" do
+  describe "POST /client" do
     it "returns http created" do
       stub_name = Faker::name
       post "/client/", params: {name:stub_name}
@@ -25,12 +28,25 @@ RSpec.describe "Clients", type: :request do
       end
     it "return a client created" do
       stub_name = Faker::name
-      # Todo: use factory
-      post "/client/", params: {name:stub_name}
-      json_response = JSON.parse(response.body)
+      expect{
+        post "/client/", params: {name:stub_name}
+      }.to change { Client.count }.from(0).to(1)
+
       expect(response).to have_http_status(:created)
-      expect(json_response['name']).to eq(stub_name)
     end
+  end
+
+  describe "DELETE /client/:id" do
+    let!(:client) { FactoryBot.create(:client) }
+
+    it "delete a client and return http ok" do
+      expect{
+        delete "/client/#{client.id}"
+      }.to change { Client.count }.from(1).to(0)
+
+      expect(response).to have_http_status(:ok)
+    end
+    
   end
 
 end
