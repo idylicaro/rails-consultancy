bddc712038def2e1458993ca740b39cc3d35dec1
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-20 14:11:19 -0300

    feat(consultants_controller): add create method

--- app/controllers/consultant_controller.rb
+++ app/controllers/consultant_controller.rb
@@ -3,4 +3,22 @@ class ConsultantController < ApplicationController
     render json: Consultant.all
   end
 
+  def create
+    #consultant = Consultant.new(name: params[:name], rating:0)
+    consultant = Consultant.new(consultant_params)
+    consultant.rating = 0;  #todo: remove for ConsultantRepository
+
+    if consultant.save
+      render json: consultant, status: :created
+    else
+      render json: consultant.errors, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def consultant_params
+    params.permit(:name)
+  end
 end
