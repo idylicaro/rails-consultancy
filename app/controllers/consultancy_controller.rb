0d77ac471d8b023eae6a4d18b03f6a9d3732a344
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-23 23:06:00 -0300

    feat(consultancy_controller): add create action

--- app/controllers/consultancy_controller.rb
+++ app/controllers/consultancy_controller.rb
@@ -2,4 +2,19 @@ class ConsultancyController < ApplicationController
   def index
     render json: Consultancy.all
   end
+
+  def create
+    client = Client.find(params[:client_id])
+    consultant = Consultant.find(params[:consultant_id])
+
+    consultancy = Consultancy.new(client:client,consultant:consultant)
+    consultancy.rating = 0;  #todo: remove for ConsultancyRepository
+
+    if consultancy.save
+      render json: consultancy, status: :created
+    else
+      render json: consultancy.errors, status: :unprocessable_entity
+    end
+
+  end
 end
