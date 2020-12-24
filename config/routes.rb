4cdfc620190dc758d7c19338d250dcfd0202231e
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-23 22:25:01 -0300

    feat(consultancy_controller): get all consultancies

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'consultant/index'
+  get 'consultancy/index'
   # 'resources :client' mapping (REST) routes
   # GET    /client        #=> index
   # GET    /client/1      #=> show
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   # POST   /client        #=> create
   # DELETE /client/1      #=> destroy
 
-  resources :client,:consultant, only:[:index, :create, :destroy, :update]
+  resources :client,:consultant,:consultancy, only:[:index, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
