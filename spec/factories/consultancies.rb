832b49dd4f8f11a34595b47f275cfed007dd4557
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-23 22:26:11 -0300

    test(consultancy_controller): ensure get all consultancies

--- spec/factories/consultancies.rb
+++ spec/factories/consultancies.rb
@@ -16,7 +16,7 @@ FactoryBot.define do
     client_id { 1 }
     consultant_id { 1 }
     start_date { "2020-12-12 01:06:51" }
-    end_date { "2020-12-12 01:06:51" }
-    rating { 1 }
+    end_date { "2021-12-12 01:06:51" }
+    rating { 0 }
   end
 end
