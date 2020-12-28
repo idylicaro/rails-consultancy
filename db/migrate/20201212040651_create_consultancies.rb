ef51d10a26b1dd3d643e6f09abdf6751bbf32f62
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-28 13:05:01 -0300

    refactor(consultancy_migrate): add closed attribute and remove startDate

--- db/migrate/20201212040651_create_consultancies.rb
+++ db/migrate/20201212040651_create_consultancies.rb
@@ -3,9 +3,9 @@ class CreateConsultancies < ActiveRecord::Migration[6.1]
     create_table :consultancies do |t|
       t.integer :client_id
       t.integer :consultant_id
-      t.datetime :start_date
       t.datetime :end_date
       t.integer :rating
+      t.boolean :closed
 
       t.timestamps
     end
