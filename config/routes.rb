11693ef092f0fe0d251b75b07fb0d97b0d0b6d82
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-30 15:59:56 -0300

    feat(category_controller): add index action

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   # DELETE /client/1      #=> destroy
   put '/consultancy/:id', to: 'consultancy#close'
 
-  resources :client,:consultant,:consultancy, only:[:index, :create, :destroy, :update]
+  resources :client,:consultant,:consultancy,:category, only:[:index, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
