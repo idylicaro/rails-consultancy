3c61b2c7cc4c661a9be88be773d3a5469ddee686
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-30 16:06:32 -0300

    feat(category_controller): add create action

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,4 +2,21 @@ class CategoryController < ApplicationController
   def index
     render json: Category.all
   end
+
+  def create
+    category = Category.new(category_params)
+
+    if category.save
+      render json: category, status: :created
+    else
+      render json: category.errors, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def category_params
+    params.permit(:name)
+  end
 end
