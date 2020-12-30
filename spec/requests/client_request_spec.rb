377c25c9cdcea369e1cb0cb249221299a94d5d5e
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-30 15:38:08 -0300

    test(client&category): ensure relationship and create client

--- spec/requests/client_request_spec.rb
+++ spec/requests/client_request_spec.rb
@@ -17,19 +17,20 @@ RSpec.describe "Clients", type: :request do
 
     it "JSON body response contains expected recipe attributes" do
       json_response = JSON.parse(response.body)
-      expect(json_response[0].keys).to match_array(['id', 'name', 'created_at', 'updated_at'])
+      expect(json_response[0].keys).to match_array(['id', 'name', 'created_at', 'updated_at','category_id'])
     end
   end
   describe "POST /client" do
+    let!(:category) { FactoryBot.create(:category) }
     it "returns http created" do
       stub_name = Faker::name
-      post "/client/", params: {name:stub_name}
+      post "/client/", params: {name:stub_name, category_id: category.id}
         expect(response).to have_http_status(:created)
       end
     it "return a client created" do
       stub_name = Faker::name
       expect{
-        post "/client/", params: {name:stub_name}
+        post "/client/", params: {name:stub_name, category_id: category.id}
       }.to change { Client.count }.from(0).to(1)
 
       expect(response).to have_http_status(:created)
