1a6bf2fb27f22432b5eb213d4cd68ed16e4e1557
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-01-01 19:47:28 -0300

    test(location_controller): ensure index action

---
+++ spec/requests/location_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Locations", type: :request do
+
+  describe "GET /location" do
+    before do
+      FactoryBot.create(:location)
+      FactoryBot.create(:location)
+      get "/location/"
+    end
+
+    it "returns all location" do
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+
+    it "JSON body response contains expected recipe attributes" do
+      json_response = JSON.parse(response.body)
+      expect(json_response[0].keys).to match_array(['id', 'cep', 'created_at', 'updated_at'])
+    end
+  end
+
+end
