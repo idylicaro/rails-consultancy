0b119be1da5f59a8c6481694fa74b4dc7624745c
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-01-01 19:52:22 -0300

    feat(location_controller): add create action

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -2,4 +2,18 @@ class LocationController < ApplicationController
   def index
     render json: Location.all
   end
+
+  def create
+    location = Location.new(location_params)
+
+    if location.save
+      render json: location, status: :created
+    else
+      render json: location.errors, status: :unprocessable_entity
+    end
+  end
+
+  def location_params
+    params.permit(:cep)
+  end
 end
