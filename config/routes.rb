c6546b89916968c576a311aef21ea69be0dfbbc5
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-01-01 19:47:07 -0300

    feat(location_controller): add index action

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   # PUT    /client/1      #=> update
   # POST   /client        #=> create
   # DELETE /client/1      #=> destroy
+  #
   put '/consultancy/:id', to: 'consultancy#close'
 
-  resources :client,:consultant,:consultancy,:category, only:[:index, :create, :destroy, :update]
+  resources :client, :consultant, :consultancy, :category, :location, only:[:index, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
