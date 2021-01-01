0d48c026b87e830989413c2ed659898199273d6f
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-01-01 19:36:40 -0300

    test(model): add test of location and add test on clients

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -15,9 +15,12 @@ RSpec.describe Client, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:category_id) }
+    it { should validate_presence_of(:location_id) }
   end
 
   describe 'association' do
     it { should have_many(:consultants).through(:consultancies) }
+    it { should belong_to :category}
+    it { should belong_to :location}
   end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -10,5 +10,11 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:cep) }
+  end
+
+  describe 'association' do
+    it { should have_one :client }
+  end
 end
