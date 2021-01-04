23ec8e968a4a678ca397fc2af84a2b28322a6816
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-01-03 22:41:39 -0300

    refactor(consultancy_controller): move utility method for helper folder

--- app/helpers/api/consultancy_helper.rb
+++ app/helpers/api/consultancy_helper.rb
@@ -1,2 +1,5 @@
 module Api::ConsultancyHelper
+  def calculate_rating(old_score, new_score, count_consultancies)
+    (old_score + ((new_score - old_score) / (count_consultancies + 1))).floor(2)
+  end
 end
